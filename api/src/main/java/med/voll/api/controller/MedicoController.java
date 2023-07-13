53bcad22fcec170be2ebf0e735f4ebdbe1a48925
Felipe_1540 <felipe_1540@outlook.com>
2023-07-13 14:29:17 -0300

    changing methods

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,2 +1,31 @@
-package med.voll.api.controller;public class MedicoController {
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedicos;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados) {
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedicos> listar() {
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    }
 }
